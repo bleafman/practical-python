b5c5d72627c2f9c90103f3eac118ddde1548d8b9
Brandon Leafman <17711952+Daxo@users.noreply.github.com>
2022-10-25 11:08:26 -0500

    initial commit

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,37 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+# User variables
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+####
+
+principle = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+current_month = 0
+
+while principle > 0:
+    if (extra_payment_start_month <= current_month and current_month <= extra_payment_end_month):
+        principle = (principle * (1 + rate/12)) - (payment + extra_payment)
+        total_paid = total_paid + payment + extra_payment
+    else:
+        principle = principle * (1 + rate/12) - payment
+        total_paid = total_paid + payment
+
+    current_month += 1
+
+    if (principle < 0):
+        refund = abs(principle)
+        total_paid = total_paid - refund
+        principle = 0
+
+    # print(round(current_month, 2), round(total_paid, 2), round(principle, 2))
+
+print(f'Total paid: ${total_paid:0.2f}')
+print('Total months:', current_month)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,34 @@
 # pcost.py
 #
 # Exercise 1.27
+from os.path import exists
+
+
+def portfolio_cost(filename):
+
+    if not exists(filename):
+        raise RuntimeError('File does not exist')
+
+    total_cost = 0
+
+    with open(filename, 'rt') as file:
+        headers = next(file).split(',')
+
+        for line in file:
+            row = line.split(',')
+
+            try:
+                shares = int(row[1])
+            except ValueError:
+                print(f'No shares were recorded for {row[0]}')
+                continue
+
+            price = float(row[2])
+
+            total_cost = total_cost + (shares * price)
+
+    return total_cost
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total Cost:', cost)

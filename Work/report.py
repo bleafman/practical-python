7593c2c117da667b6b537a2e21eeca7e22975044
Brandon Leafman <17711952+Daxo@users.noreply.github.com>
2022-10-27 10:36:14 -0500

    End of 2.7

--- Work/report.py
+++ Work/report.py
@@ -37,3 +37,27 @@ def read_prices(filename):
                 prices[stock] = price
 
     return prices
+
+
+def total_gains(portfolio, prices):
+    return current_value(portfolio, prices) - portfolio_cost(portfolio)
+
+
+def portfolio_cost(portfolio):
+
+    total_cost = 0.0
+
+    for holding in portfolio:
+        total_cost = holding['price'] * holding['shares']
+
+    return total_cost
+
+
+def current_value(portfolio, prices):
+
+    total_value = 0.0
+
+    for holding in portfolio:
+        total_value += prices[holding['name']] * holding['shares']
+
+    return total_value

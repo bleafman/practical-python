e6b298fda1ec5474a5249d832605ee01aa24f077
Brandon Leafman <17711952+Daxo@users.noreply.github.com>
2022-10-31 12:45:29 -0500

    End of 2.3

--- Work/report.py
+++ Work/report.py
@@ -2,6 +2,7 @@
 #
 # Exercise 2.4
 import csv
+from operator import itemgetter
 
 
 def read_portfolio(filename):
@@ -37,6 +38,39 @@ def read_prices(filename):
                 prices[stock] = price
 
     return prices
+
+
+def make_report(portfolio, prices):
+
+    report = []
+
+    for holding in portfolio:
+
+        name, shares, price = itemgetter('name', 'shares', 'price')(holding)
+        current_price = prices.get(name)
+
+        change = current_price - price
+
+        entry = (name, shares, price, change)
+
+        report.append(entry)
+
+    return report
+
+
+def print_report(report=make_report(read_portfolio('Data/portfolio.csv'), read_prices('Data/prices.csv'))):
+
+    headers = ('Name', 'Shares', 'Price', 'Change')
+
+    print(
+        f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+
+    empty_space = ''
+
+    print(f'{empty_space:_>10s} {empty_space:_>10s} {empty_space:_>10s} {empty_space:_>10s}')
+
+    for name, shares, price, change in report:
+        print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
 
 
 def total_gains(portfolio, prices):
